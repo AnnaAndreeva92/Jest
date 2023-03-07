43d3c7bf766bb66735b0d7190a2497fc8c535156
AnnaAndreeva92 <ann-stesha@yandex.ru>
2023-03-08 01:00:35 +0300

    all files added

---
+++ jest/test/unit/sortByName.test.js
@@ -0,0 +1,32 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города"
+    ]
+      
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]
+    const result = sorting.sortByName(input)
+
+    expect(result).toEqual(expected);
+  });
+});
+
+it("Books names should be sorted in ascending order", () => {
+  expect(
+    sorting.sortByName([
+      "Властелин Колец",
+      "Властелин Колец"
+    ])
+  ).toEqual([
+    "Властелин Колец",
+    "Властелин Колец"
+  ]);
+});
